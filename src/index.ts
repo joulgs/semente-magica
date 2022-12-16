ec806e3223db9ae8d66de1dedc1db3c745eb5eb1
Jonathas Soares <joul.gs@gmail.com>
2022-12-16 07:05:05 -0300

    inicio do servidor express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+import express from 'express';
+
+// inicia um servidor express
+const app = express();
+
+// encontra o ip local da maquina
+ 
+// define a porta do servidor
+const port = 3000;
+// define a rota inicial
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+// inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+
+
+
+
+// 
+
 console.log("Eu..? Eu Estou Vivo Muahahahaha !!!")
